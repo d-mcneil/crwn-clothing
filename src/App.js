7a339645abea1220289ba16fe01bbc8efe5654cd
Dean McNeil <deanolmcneil@gmail.com>
2023-02-17 16:20:09 -0500

    Add Navigation component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import Directory from "./components/directory/directory.component";
+import { Routes, Route } from "react-router-dom";
+
+import Navigation from "./routes/navigation/navigation.component";
+import Home from "./routes/home/home.component";
 
 const App = () => {
-  return <Directory />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
